6f0b4a07bef0fc67c920957548bef979baf11aa4
Oleg Subachev <solsw@posolsoft.com>
2021-11-12 00:04:46 +0500

    v0.2.0

---
+++ select.go
@@ -0,0 +1,68 @@
+//go:build go1.18
+
+package go2linq
+
+// Reimplementing LINQ to Objects: Part 3 – "Select" (and a rename...)
+// https://codeblog.jonskeet.uk/2010/12/23/reimplementing-linq-to-objects-part-3-quot-select-quot-and-a-rename/
+// https://docs.microsoft.com/dotnet/api/system.linq.enumerable.select
+
+// Select projects each element of a sequence into a new form.
+// Select panics if 'source' or 'selector' is nil.
+func Select[Source, Result any](source Enumerator[Source], selector func(Source) Result) Enumerator[Result] {
+	if source == nil {
+		panic(ErrNilSource)
+	}
+	if selector == nil {
+		panic(ErrNilSelector)
+	}
+	return OnFunc[Result]{
+		MvNxt: func() bool { return source.MoveNext() },
+		Crrnt: func() Result { return selector(source.Current()) },
+		Rst: func() { source.Reset() },
+	}
+}
+
+// SelectErr is like Select but returns an error instead of panicking.
+func SelectErr[Source, Result any](source Enumerator[Source], selector func(Source) Result) (res Enumerator[Result], err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			e, ok := x.(error)
+			if ok {
+				res = nil
+				err = e
+			}
+		}
+	}()
+	return Select(source, selector), nil
+}
+
+// SelectIdx projects each element of a sequence into a new form by incorporating the element's index.
+// SelectIdx panics if 'source' or 'selector' is nil.
+func SelectIdx[Source, Result any](source Enumerator[Source], selector func(Source, int) Result) Enumerator[Result] {
+	if source == nil {
+		panic(ErrNilSource)
+	}
+	if selector == nil {
+		panic(ErrNilSelector)
+	}
+	var i int = -1 // position before the first element
+	return OnFunc[Result]{
+		MvNxt: func() bool { i++; return source.MoveNext() },
+		Crrnt: func() Result { return selector(source.Current(), i) },
+		Rst: func() { i = -1; source.Reset() },
+	}
+}
+
+// SelectIdxErr is like SelectIdx but returns an error instead of panicking.
+func SelectIdxErr[Source, Result any](source Enumerator[Source], selector func(Source, int) Result) (res Enumerator[Result], err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			e, ok := x.(error)
+			if ok {
+				res = nil
+				err = e
+			}
+		}
+	}()
+	return SelectIdx(source, selector), nil
+}
---
+++ select_test.go
@@ -0,0 +1,190 @@
+//go:build go1.18
+
+package go2linq
+
+import (
+	"fmt"
+	"testing"
+)
+
+// https://github.com/jskeet/edulinq/blob/master/src/Edulinq.Tests/SelectTest.cs
+
+func Test_SelectErr_int_int(t *testing.T) {
+	var count int
+	type args struct {
+		source Enumerator[int]
+		selector func(int) int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Enumerator[int]
+		wantErr bool
+		expectedErr error
+	}{
+		{name: "NullSourceThrowsNullArgumentException",
+			args: args{
+				selector: func(x int) int { return x + 1 },
+			},
+			wantErr: true,
+			expectedErr: ErrNilSource,
+		},
+		{name: "NullProjectionThrowsNullArgumentException",
+			args: args{
+				source: NewOnSlice(1, 3, 7, 9, 10),
+			},
+			wantErr: true,
+			expectedErr: ErrNilSelector,
+		},
+		{name: "SimpleProjection",
+			args: args{
+				source: NewOnSlice(1, 5, 2),
+				selector: func(x int) int { return x * 2 },
+			},
+			want: NewOnSlice(2, 10, 4),
+		},
+		{name: "EmptySource",
+			args: args{
+				source: Empty[int](),
+				selector: func(x int) int { return x * 2 },
+			},
+			want: Empty[int](),
+		},
+		{name: "SideEffectsInProjection1",
+			args: args{
+				source: NewOnSlice(3, 2, 1), // Actual values won't be relevant
+				selector: func(int) int { count++; return count },
+			},
+			want: NewOnSlice(1, 2, 3),
+		},
+		{name: "SideEffectsInProjection2",
+			args: args{
+				source: NewOnSlice(1, 2, 3), // Actual values won't be relevant
+				selector: func(int) int { count++; return count },
+			},
+			want: NewOnSlice(4, 5, 6),
+		},
+		{name: "SideEffectsInProjection3",
+			args: args{
+				source: NewOnSlice(1, 2, 3), // Actual values won't be relevant
+				selector: func(int) int { count++; return count },
+			},
+			want: NewOnSlice(11, 12, 13),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SelectErr(tt.args.source, tt.args.selector)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SelectErr() error = '%v', wantErr '%v'", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err != tt.expectedErr {
+					t.Errorf("SelectErr() error = '%v', expectedErr '%v'", err, tt.expectedErr)
+				}
+				return
+			}
+			if !SequenceEqual(got, tt.want) {
+				got.Reset()
+				tt.want.Reset()
+				t.Errorf("SelectErr() = '%v', want '%v'", String(got), String(tt.want))
+			}
+		})
+		if tt.name == "SideEffectsInProjection2" {
+			count = 10
+		}
+	}
+}
+
+func Test_SelectIdxErr_int_int(t *testing.T) {
+	type args struct {
+		source Enumerator[int]
+		selector func(int, int) int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Enumerator[int]
+		wantErr bool
+		expectedErr error
+	}{
+		{name: "WithIndexNullSourceThrowsNullArgumentException",
+			args: args{
+				selector: func(x, index int) int { return x + index },
+			},
+			wantErr: true,
+			expectedErr: ErrNilSource,
+		},
+		{name: "WithIndexNullPredicateThrowsNullArgumentException",
+			args: args{
+				source: NewOnSlice(1, 3, 7, 9, 10),
+			},
+			wantErr: true,
+			expectedErr: ErrNilSelector,
+		},
+		{name: "WithIndexSimpleProjection",
+			args: args{
+				source: NewOnSlice(1, 5, 2),
+				selector: func(x, index int) int { return x + index*10 },
+			},
+			want: NewOnSlice(1, 15, 22),
+		},
+		{name: "WithIndexEmptySource",
+			args: args{
+				source: Empty[int](),
+				selector: func(x, index int) int { return x + index },
+			},
+			want: Empty[int](),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SelectIdxErr(tt.args.source, tt.args.selector)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SelectIdxErr() error = '%v', wantErr '%v'", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err != tt.expectedErr {
+					t.Errorf("SelectIdxErr() error = '%v', expectedErr '%v'", err, tt.expectedErr)
+				}
+				return
+			}
+			if !SequenceEqual(got, tt.want) {
+				got.Reset()
+				tt.want.Reset()
+				t.Errorf("SelectIdxErr() = '%v', want '%v'", String(got), String(tt.want))
+			}
+		})
+	}
+}
+
+func Test_Select_int_string(t *testing.T) {
+	type args struct {
+		source Enumerator[int]
+		selector func(int) string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Enumerator[string]
+	}{
+		{name: "SimpleProjectionToDifferentType",
+			args: args{
+				source: NewOnSlice(1, 5, 2),
+				selector: func(x int) string { return fmt.Sprint(x) },
+			},
+			want: NewOnSlice("1", "5", "2"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Select(tt.args.source, tt.args.selector); !SequenceEqual(got, tt.want) {
+				got.Reset()
+				tt.want.Reset()
+				t.Errorf("Select() = '%v', want '%v'", String(got), String(tt.want))
+			}
+		})
+	}
+}
