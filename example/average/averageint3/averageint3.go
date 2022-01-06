cec9ebc1c2c2472b1e8e0808f0c799a4a7bf5546
Oleg Subachev <solsw@posolsoft.com>
2022-01-06 16:56:07 +0500

    example/average, example/distinct added

---
+++ example/average/averageint3/averageint3.go
@@ -0,0 +1,19 @@
+//go:build go1.18
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/solsw/go2linq"
+)
+
+// see example from Enumerable.Average help
+// https://docs.microsoft.com/dotnet/api/system.linq.enumerable.average
+
+func main() {
+	numbers := go2linq.NewOnSliceEn("10007", "37", "299846234235")
+	average := go2linq.AverageIntegerMust(numbers, func(e string) int { r, _ := strconv.Atoi(e); return r })
+	fmt.Printf("The average is %.f.\n", average)
+}
