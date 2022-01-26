be535750833b31b02d112d3cd3c62418d3ff4b1f
Oleg Subachev <solsw@posolsoft.com>
2022-01-26 10:21:47 +0500

    example/concat, example/contains, example/count added

---
+++ example/contains/contains/contains.go
@@ -0,0 +1,25 @@
+//go:build go1.18
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/solsw/go2linq/v2"
+)
+
+// see the first example from Enumerable.Contains help
+// https://docs.microsoft.com/dotnet/api/system.linq.enumerable.contains
+
+func main() {
+	fruits := go2linq.NewEnSlice("apple", "banana", "mango", "orange", "passionfruit", "grape")
+	fruit := "mango"
+	hasMango := go2linq.ContainsMust(fruits, fruit)
+	var what string
+	if hasMango {
+		what = "does"
+	} else {
+		what = "does not"
+	}
+	fmt.Printf("The array %s contain '%s'.\n", what, fruit)
+}
