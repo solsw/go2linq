76ea480ad9841c5cbab3da7d498c6694a7a77dd0
Oleg Subachev <git@solsw.ru>
2023-03-22 21:03:41 +0500

    imported generichelper modified

--- enumerator.go
+++ enumerator.go
@@ -51,7 +51,7 @@ func enrToStringFmt[T any](enr Enumerator[T], sep, lrim, rrim, ledge, redge stri
 	if stringer, ok := enr.(fmt.Stringer); ok {
 		return stringer.String()
 	}
-	isStringer := generichelper.TypeIsType[T, fmt.Stringer]()
+	isStringer := generichelper.TypeHoldsType[T, fmt.Stringer]()
 	var b strings.Builder
 	for enr.MoveNext() {
 		if b.Len() > 0 {
@@ -64,7 +64,7 @@ func enrToStringFmt[T any](enr Enumerator[T], sep, lrim, rrim, ledge, redge stri
 
 // enrToStringEnr converts the sequence to [Enumerator[string]].
 func enrToStringEnr[T any](enr Enumerator[T]) Enumerator[string] {
-	isStringer := generichelper.TypeIsType[T, fmt.Stringer]()
+	isStringer := generichelper.TypeHoldsType[T, fmt.Stringer]()
 	return enrFunc[string]{
 		mvNxt: func() bool { return enr.MoveNext() },
 		crrnt: func() string { return asStringPrim(enr.Current(), isStringer) },
